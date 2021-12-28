fcecc13e4196ddcc77bdf8f3359ea100da5db89b
Jakub Malczak <jmalczak@gmail.com>
2021-12-28 13:53:34 +0100

    Added dotnet tools to fish path

--- fish/config.fish
+++ fish/config.fish
@@ -1,6 +1,9 @@
 set -g -x TERM "xterm-256color" 
 set -g -x LC_CTYPE "UTF-8" 
 
+fish_add_path /opt/homebrew/sbin
+fish_add_path ~/.dotnet/tools
+
 if status is-interactive
 	# Set vi mode
 	fish_vi_key_bindings
